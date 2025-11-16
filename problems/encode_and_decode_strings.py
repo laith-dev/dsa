d72947c1e2b3eb98602fa206565a2e6983a01e37
Laith Al-Zou'bi <laithdev97@gmail.com>
2025-11-16 17:11:32 +0300

    feat: add solution to the "Encode and Decode Strings" problem

--- problems/encode_and_decode_strings.py
+++ problems/encode_and_decode_strings.py
@@ -13,3 +13,32 @@ class Solution1:
 
     def decode(self, s: str) -> list[str]:
         return self.s
+
+
+class Solution2:
+    """
+    Using a non-UTF8 character as a separator.
+    """
+
+    SEPARATOR = "\xFF"
+
+    def encode(self, strs: list[str]) -> str:
+        """
+        Complexity:
+        - Time:  O(n); the joining operation.
+        - Space: O(1)
+        """
+
+        self.strs_len = len(strs)
+        return self.SEPARATOR.join(strs)
+
+    def decode(self, s: str) -> list[str]:
+        """
+        Complexity:
+        - Time:  O(n); the splitting operation.
+        - Space: O(n); the splitting operation.
+        """
+
+        if not s and self.strs_len > 0:
+            return [''] * self.strs_len
+        return s.split(self.SEPARATOR) if s else []
