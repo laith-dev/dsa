08134f64d84689193a27f569ef2b0a6803764730
Laith Al-Zou'bi <laithdev97@gmail.com>
2025-11-20 21:56:49 +0300

    feat: add a fourth solution to the "Is Anagram" problem

--- problems/is_anagram.py
+++ problems/is_anagram.py
@@ -60,3 +60,25 @@ class Solution2:
 class Solution3:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
+
+
+class Solution4:
+    """
+    Complexity:
+    - Time:  O(n)
+    - Space: O(1)
+    """
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for val in count:
+            if val != 0:
+                return False
+        return True
